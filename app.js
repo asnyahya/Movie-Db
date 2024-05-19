fd1b03899f72e78a599f25e54d03bb5b8660a27b
asnyahya <mohasnawi545@gmail.com>
2024-05-19 17:26:23 +0700

    Menambahkan fitur modal detail

--- app.js
+++ app.js
@@ -16,14 +16,45 @@ $.ajax({
             <h6 class="card-subtitle mb-2 text-muted">${el.Year}</h6>
             <a
               href="#"
-              class="btn btn-primary"
-              >Show Details</a
-            >
+              class="btn btn-primary mov-detail-button" data-bs-toggle="modal" data-bs-target="#movDetail" data-imdbid="${el.imdbID}">Show Details</a
+            > 
           </div>
         </div>
       </div>`;
     });
     $('.mov-container').html(card);
+    $('.mov-detail-button').on('click', function () {
+      $.ajax({
+        url:
+          'http://www.omdbapi.com/?apikey=6dd6389&i=' + $(this).data('imdbid'),
+        success: (el) => {
+          const movDetail = `<div class="container-fluid">
+          <div class="row">
+            <div class="col-md-3">
+              <img
+                src="${el.Poster}"
+                alt="${el.Title}"
+                class="img-fluid"
+              />
+            </div>
+            <div class="col-md">
+              <ul class="list-group">
+                <li class="list-group-item"><h4>${el.Title} (${el.Year})</h4></li>
+                <li class="list-group-item"><strong>Genre: </strong>${el.Genre}</li>
+                <li class="list-group-item"><strong>Director: </strong>${el.Director}</li>
+                <li class="list-group-item"><strong>Actors: </strong>${el.Actors}</li>
+                <li class="list-group-item">
+                  <strong>Plot: </strong><br />${el.Plot}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>`;
+
+          $('.modal-body').html(movDetail);
+        },
+      });
+    });
   },
   error: (err) => {
     console.log(err.responseText);
