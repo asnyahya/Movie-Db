b4aadc64051d48730b58ee7c4b4360f24432e5c0
asnyahya <mohasnawi545@gmail.com>
2024-05-22 13:58:00 +0700

    Refactor kode dengan async await pada file app.js biar lebih bersih dan rapi

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 const searchBtn = document.querySelector('.search-button');
-searchBtn.addEventListener('click', function () {
+searchBtn.addEventListener('click', async function () {
   const inputKeyword = document.querySelector('.input-keyword');
-  fetch('http://www.omdbapi.com/?apikey=6dd6389&s=' + inputKeyword.value)
-    .then((response) => response.json())
-    .then((response) => {
-      const movies = response.Search;
-      let card = '';
-      movies.forEach((el) => (card += cardBox(el)));
-      const movContainer = document.querySelector('.mov-container');
-      movContainer.innerHTML = card;
-
-      const detailBtn = document.querySelectorAll('.mov-detail-button');
-      detailBtn.forEach((btn) => {
-        btn.addEventListener('click', function () {
-          const imdbid = this.dataset.imdbid;
-          fetch('http://www.omdbapi.com/?apikey=6dd6389&i=' + imdbid)
-            .then((response) => response.json())
-            .then((el) => {
-              const showDetails = showDetail(el);
-              const modalBody = document.querySelector('.modal-body');
-              modalBody.innerHTML = showDetails;
-            });
-        });
-      });
-    });
+  const movie = await getMovie(inputKeyword.value);
+  updateUI(movie);
 });
+
+document.addEventListener('click', async function (e) {
+  if (e.target.classList.contains('mov-detail-button')) {
+    const imdbid = e.target.dataset.imdbid;
+    getShowDetail(imdbid)
+      .then((movieDetail) => uiDetail(movieDetail))
+      .catch((error) => console.error(error));
+  }
+});
+
+function getShowDetail(imdbid) {
+  return fetch('http://www.omdbapi.com/?apikey=6dd6389&i=' + imdbid)
+    .then((response) => response.json())
+    .then((el) => el);
+}
+
+function uiDetail(el) {
+  const showDetails = showDetail(el);
+  const modalBody = document.querySelector('.modal-body');
+  modalBody.innerHTML = showDetails;
+}
+
+function getMovie(keyword) {
+  return fetch('http://www.omdbapi.com/?apikey=6dd6389&s=' + keyword)
+    .then((response) => response.json())
+    .then((response) => response.Search);
+}
+
+function updateUI(movie) {
+  let card = '';
+  movie.forEach((el) => (card += cardBox(el)));
+  const movContainer = document.querySelector('.mov-container');
+  movContainer.innerHTML = card;
+}
 
 function cardBox(el) {
   return `<div class="col-md-4 my-3">
@@ -32,7 +45,7 @@ function cardBox(el) {
               <img
                 src="${el.Poster}"
                 class="card-img-top"
-                alt="${el.Title}
+                alt="${el.Title}"
               />
               <div class="card-body">
                 <h5 class="card-title">${el.Title}</h5>
